bd8075fbaf0c72e63a757f4096101c69b46c9756
daitugu <132569322+daitugu@users.noreply.github.com>
2023-11-23 14:42:53 +0000

    更新

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -9,11 +9,18 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
   def index
+    @post_images = PostImage.all
+  end
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_images_path
+  end
+  def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
-  def show
-  end
-  
   private
 
   def post_image_params
