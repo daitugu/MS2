a04f3e12793a71ea2db60d224f4e4ddb216e83fb
daitugu <132569322+daitugu@users.noreply.github.com>
2023-11-25 12:55:01 +0000

    23

--- config/routes.rb
+++ config/routes.rb
@@ -10,3 +10,5 @@ Rails.application.routes.draw do
   resources :users,only:[:show,:edit,:update]
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
